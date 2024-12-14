979eb88025373959b045e0bd50a8edeec1a4e9bc
EricsonCR <ericson4634@gmail.com>
2024-12-13 20:41:18 -0500

    cuarto commit

--- src/app/service/reporte.service.ts
+++ src/app/service/reporte.service.ts
@@ -16,20 +16,28 @@ export class ReporteService {
   generarPDF(pago: Pago, pagoDetalle: PagoDetalle[]) {
     const doc = new jsPDF();
 
-    // Título del reporte
+    // Título
     doc.setFontSize(24);
     doc.text('Comprobante de Pago', 60, 10);
 
+    //Subtitulo
     doc.setFontSize(18);
     doc.text('Datos de Pago', 10, 20);
 
     // Información del Pago
     doc.setFontSize(12);
-    doc.text(`Número de Pago: ${pago.id}`, 20, 30);
+    doc.text("Numero de Pago:", 20, 30); doc.text(pago.id.toString(), 80, 30);
+    doc.text("Numero de Matricula:", 20, 40); doc.text(pago.id_matricula.toString(), 80, 40);
+    doc.text("Metodo de Pago:", 20, 50); doc.text(pago.metodo_pago.toString(), 80, 50);
+    doc.text("Numero de OP:", 20, 60); doc.text(pago.numero_op.toString(), 80, 60);
+    doc.text("Fecha de Pago:", 20, 70); doc.text(formatDate(pago.registro, 'dd-MM-yyyy', 'en-US').toString(), 80, 70);
+
+
+    /* doc.text(`Número de Pago: ${pago.id}`, 20, 30);
     doc.text(`Numero Matricula: ${pago.id_matricula}`, 20, 40);
     doc.text(`Metodo Pago: ${pago.metodo_pago}`, 20, 50);
     doc.text(`Numero OP: ${pago.numero_op}`, 20, 60);
-    doc.text(`Fecha de Pago: ${formatDate(pago.registro, 'dd-MM-yyyy', 'en-US')}`, 20, 70);
+    doc.text(`Fecha de Pago: ${formatDate(pago.registro, 'dd-MM-yyyy', 'en-US')}`, 20, 70); */
 
     // Detalles de Pago
     doc.setFontSize(18);
